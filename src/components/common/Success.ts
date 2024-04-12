189b84dc92552401df72df5832024117cfdd89ef
sylotana <anatolyshangin@mail.ru>
2024-04-12 16:15:28 +0300

    feat: Добавление общего компонента Success

---
+++ src/components/common/Success.ts
@@ -0,0 +1,28 @@
+import { Component } from '../base/Component';
+import { ensureElement } from '../../utils/utils';
+
+interface ISuccess {
+  total: number;
+}
+
+interface ISuccessActions {
+  onClick: () => void;
+}
+
+export class Success extends Component<ISuccess> {
+  protected _close: HTMLButtonElement;
+  protected _total: HTMLParagraphElement;
+
+  constructor(container: HTMLElement, actions: ISuccessActions, total: number) {
+    super(container);
+
+    this._close = ensureElement<HTMLButtonElement>('.order-success__close', this.container);
+    this._total = ensureElement<HTMLParagraphElement>('.order-success__description', this.container);
+
+    this._total.textContent = `Списано ${total} синапсов`;
+
+    if (actions?.onClick) {
+      this._close.addEventListener('click', actions.onClick);
+    }
+  }
+}
